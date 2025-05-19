bdf59f70cc5479257ade671cd035c583898fefc3
Yuvraaj Limbole <yuvraj.limbole@aigenomics.com>
2025-05-19 18:17:36 +0530

    dark shadow & half shadow image enhancing technique going on

--- main.py
+++ main.py
@@ -176,9 +176,10 @@ def main():
     saved_img_dir = f"{output_dir}/{timestamp_str}"
 
     # Enhanced usage including overlay visualization
-    calibrator = CircleColorGlobalCalibrator('reference_data/ref_shadow_img.png')
+    calibrator = CircleColorGlobalCalibrator('reference_data/ref_colors_data.json')
     # image_path = 'reference_data/refernce_image.jpg'
-    image_path = 'reference_data/refe.jpeg'
+    # image_path = 'reference_data/ref_shadow_img.png'
+    image_path = 'reference_data/P301C_100_Shadow.png'
     output_enhanced = f'{saved_img_dir}/enhanced_{timestamp_str}.jpeg'
     output_overlay = f'{saved_img_dir}/annotated_{timestamp_str}.jpeg'
 
